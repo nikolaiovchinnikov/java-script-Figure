103e8fa5f88dc2d9d3d499e499f48ed1cffecd51
Ovchinnikov Nikolay <slavanikolu.gmail.com>
2022-11-04 20:23:24 +0600

    добавил методы для вычесления площади круга

--- ts/main.ts
+++ ts/main.ts
@@ -1,4 +1,39 @@
+type num = number
+type str = string
+type bool = boolean
 
+abstract class Figure {
+    constructor(){
+    }
+    public abstract calculateArea ():num
+}
+class Circle extends Figure{
+    pi:num = Math.PI
+    constructor(){
+        super()
+    }
+    public override calculateArea (...args:Array<str | num>):num{
+        if(args[0] ==="r"){
+            return this.pi * (<num>args[1] * <num>args[1])
+        }
+        else if (args[0] ==="d"){
+            return ((<num>args[1] * <num>args[1]) / 4) * this.pi;
+        }
+        else if (args[0] ==="l"){
+            return (<num>args[1] * <num>args[1]) / (4 * this.pi);
+        }
+        return 1
+    }
+}
+
+
+
+
+
+
+
+const circle: Circle = new Circle ()
+console.log( circle.calculateArea("l",5))
 
 
 
