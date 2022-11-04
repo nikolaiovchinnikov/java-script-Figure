5f1dd46a23203317d4bb17071f2d9f9378dcf8ec
Ovchinnikov Nikolay <slavanikolu.gmail.com>
2022-11-04 21:08:32 +0600

    добавил методы нахождения площади прямоугольника

--- ts/main.ts
+++ ts/main.ts
@@ -25,16 +25,30 @@ class Circle extends Figure{
         return 1
     }
 }
+class Square extends Figure{
+    constructor(){
+        super ()
+    }
+    public override calculateArea (...args:Array<str | num>):num{
+        if(typeof(args[0]) !== "string"){
+            return <num>args[0] * (<num>args[1] || <num>args [0])
+        }
+        else if(args[0] === "d"){
+            return (<num>args[1] * <num>args[1]) / 2
+        }return 1
+        
+    }
+}
 
 
 
 
 
 
-
+const square: Square = new Square ()
 const circle: Circle = new Circle ()
 console.log( circle.calculateArea("l",5))
-
+console.log( square.calculateArea("d",4))
 
 
 
